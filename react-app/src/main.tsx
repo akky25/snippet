e37e8253608f79a0ba008ecb04bc16d041a91693
akky25 <ry.akita1991@gmail.com>
2024-04-01 08:44:32 +0900

    tmp追加

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Component } from "./snippets/Tmp";
 // import Component from "./snippets/ContactForm/index.tsx";
-import Component from "./snippets/HolyGrail";
+// import Component from "./snippets/HolyGrail";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Component />
+    <Component count={false} />
   </React.StrictMode>
 );
