20b69052619aeae8d53905bcf0fb7c51a9bb9862
akky25 <ry.akita1991@gmail.com>
2024-04-25 20:56:38 +0900

    merge sort

--- node/src/algorithm/merge_sort/index.test.ts
+++ node/src/algorithm/merge_sort/index.test.ts
@@ -2,7 +2,7 @@ import { sort } from ".";
 
 test("tmp", () => {
   const arr = [5, 3, 8, 4, 6];
-  const res = sort(arr, 0, arr.length - 1);
+  const res = sort(arr);
 
-  expect(res).toEqual(arr.toSorted((a, b) => a - b));
+  expect(res).toEqual([3, 4, 5, 6, 8]);
 });
--- node/src/algorithm/merge_sort/index.ts
+++ node/src/algorithm/merge_sort/index.ts
@@ -1,38 +1,35 @@
 function merge(arr1: number[], arr2: number[]) {
-  // console.log(arr1, arr2);
-  const length = arr1.length + arr2.length;
-  let newArr = [];
-  for (let i = 0, j1 = 0, j2 = 0; i < length; i++) {
-    if (arr1[j1] < arr2[j2]) {
-      newArr[i] = arr1[j1];
-      j1++;
-    } else if (arr1[j1] > arr2[j2]) {
-      newArr[i] = arr2[j2];
-      j2++;
+  const result: number[] = [];
+  let index1 = 0;
+  let index2 = 0;
+
+  while (index1 < arr1.length && index2 < arr2.length) {
+    if (arr1[index1] < arr2[index2]) {
+      result.push(arr1[index1]);
+      index1++;
     } else {
-      newArr[i] = arr1[j1];
-      j1++;
-      newArr[i + 1] = arr2[j2];
-      j2++;
-
-      i++;
-    }
-
-    if (j1 === arr1.length) {
-      newArr = [...newArr, ...arr2.slice(j2)];
-      break;
-    } else if (j2 === arr2.length) {
-      newArr = [...newArr, ...arr1.slice(j1)];
-      break;
+      result.push(arr2[index2]);
+      index2++;
     }
   }
-  return newArr;
+
+  if (index1 < arr1.length) {
+    result.push(...arr1.slice(index1));
+  } else if (index2 < arr2.length) {
+    result.push(...arr2.slice(index2));
+  }
+
+  return result;
 }
 
-export function sort(arr: number[], left: number, right: number) {
-  console.log(arr.slice(left, right + 1));
-  if (left === right) return [arr[left]];
-  const mid = Math.floor((left + right) / 2);
-  const res = merge(sort(arr, left, mid), sort(arr, mid + 1, right));
-  return res;
+export function sort(array: number[]): number[] {
+  const mid = Math.floor(array.length / 2);
+
+  if (mid < 1) return array;
+
+  const leftArr = array.slice(0, mid);
+  const rightArr = array.slice(mid);
+  const result = merge(sort(leftArr), sort(rightArr));
+
+  return result;
 }
