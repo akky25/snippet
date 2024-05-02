cf2008af2f60138d62e0b3512e13200cabb544f0
akky25 <ry.akita1991@gmail.com>
2024-05-02 16:59:41 +0900

    todo

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Component from "./snippets/TemperatureConverter";
+// import Component from "./snippets/TemperatureConverter";
 // import { Component } from "./snippets/Tmp";
 // import Component from "./snippets/ContactForm/index.tsx";
 // import Component from "./snippets/HolyGrail";
+import Component from "./snippets/ToDo";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
