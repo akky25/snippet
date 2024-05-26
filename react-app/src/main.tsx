ee647fd8ba19b850222b2f92b98d3f6b46361313
akky25 <ry.akita1991@gmail.com>
2024-05-26 12:33:59 +0900

    MSW追加

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Component } from "./snippets/Tmp";
 // import Component from "./snippets/TemperatureConverter";
 // import { Component } from "./snippets/Tmp";
 // import Component from "./snippets/ContactForm/index.tsx";
 // import Component from "./snippets/HolyGrail";
-import Component from "./snippets/ToDo";
+// import Component from "./snippets/ToDo";
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Component />
-  </React.StrictMode>
-);
+  const { worker } = await import("./mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <Component />
+    </React.StrictMode>
+  );
+});
