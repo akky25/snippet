54e82130475abe2be8b8531765aa91771f0b1e9c
akky25 <ry.akita1991@gmail.com>
2024-07-28 16:44:10 +0900

    add pancake layout

---
+++ react-app/src/snippets/layout/Pancake/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css";
+
+function Component() {
+  return (
+    <div className={styles.root}>
+      <header>Header</header>
+      <main>Main</main>
+      <footer>Footer Content</footer>
+    </div>
+  );
+}
+
+export default Component;
