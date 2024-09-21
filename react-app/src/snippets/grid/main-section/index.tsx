235ea05977e428b48ffa3d3697579de9e69243c4
akky25 <ry.akita1991@gmail.com>
2024-09-21 16:26:32 +0900

    add grid main section

---
+++ react-app/src/snippets/grid/main-section/index.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import styles from "./index.module.css";
+
+export const MainSection = ({
+  type,
+}: {
+  type: "1" | "2" | "2-1" | "3" | "4";
+}) => {
+  const sectionClassName = clsx([
+    type === "1" && ["section-01", styles.section01],
+    type === "2" && ["section-02", styles.section02],
+    type === "2-1" && ["section-02-1", styles.section02_1],
+    type === "3" && ["section-03", styles.section03],
+  ]);
+
+  return (
+    <>
+      <div className={sectionClassName}>
+        <h2 className={clsx("heading")}>データを活用してプランを作ろう</h2>
+        <p className={clsx("text")}>
+          日々の活動データとグローバルに集積されたデータを元に効果的に目標達成を達成できるプランを作成
+        </p>
+        <button className={clsx("button")}>無料ではじめる</button>
+        <img
+          className={clsx("photo img-fill")}
+          src="/public/grid-assets/rome/1.jpg"
+          alt=""
+          width="1724"
+          height="2218"
+        />
+      </div>
+    </>
+  );
+};
